d0f4eb2efe77f1f90a92c8988314703c4190e72c
sahilnare <sahilnare78@gmail.com>
2019-06-11 14:58:07 +0530

    Additional Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoading: false,
+      character: {}
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true
+    })
+    fetch("https://swapi.co/api/starships/11")
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          isLoading: false,
+          character: res
+        })
+      })
+  }
+
+  render() {
+    const starshipInfo = <div> 
+      <h2>Name: {this.state.character.name}</h2>
+      <h2>Model: {this.state.character.model}</h2>
     </div>
-  );
+    const displayText = this.state.isLoading ? "Loading..." : starshipInfo
+    return (
+      <div className="App">
+        <h1>{displayText}</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
